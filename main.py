4aa3abc18960e94981ba57cd529fe937d080a15f
nstoddard <nstoddard@proton.me>
2024-10-28 22:02:42 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting asteroids!")
+    print(f"Screen width: " + str(SCREEN_WIDTH))
+    print(f"Screen height: " + str(SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
